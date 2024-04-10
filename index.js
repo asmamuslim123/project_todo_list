bbf61f4f1509fb02402f301409414bcd14113610
Miss Asma <161770475+asmamuslim123@users.noreply.github.com>
2024-04-11 00:24:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "what you want to add in your Todos?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add more?",
+            default: "false"
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
